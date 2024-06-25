80bcca2552255694e94bb56bd820a90a75b921c8
NalinPlad <43052612+NalinPlad@users.noreply.github.com>
2024-06-25 11:19:18 -0700

    search function

--- src/components/AsmManual.svelte
+++ src/components/AsmManual.svelte
@@ -1,19 +1,21 @@
----
-import man from './AsmManualData.json';
+<script>
+    import man from './AsmManualData.json';
 
-let search = "";
----
-<div class="w-80 h-96 bg-amber-900 p-3 font-mono rounded-lg shadow-lg text-right flex flex-col">
+    let search = "";
+</script>
+
+<div class="w-80 h-96 bg-amber-900 p-3 font-mono rounded-lg shadow-xl text-right flex flex-col">
     <div class="flex flex-col items-end">
         <h1 class="text-amber-100 text-md w-2/3">The x86 Assembly Instruction Set</h1>
         <p class="text-xs">INTEL, 1995</p>
     </div>
     
     <hr class="w-full h-1 bg-amber-100 my-2 rounded-full"/>
-    <input type="text" placeholder="Search..." value={search} class="input mb-2"/>
+
+    <input type="text" placeholder="Search..." bind:value={search} class="input mb-2"/>
+
     <div class="flex w-full flex-col text-left h-full overflow-scroll bg-amber-950 p-5 gap-y-5 rounded-xl">
-        {
-            man.instructions.filter((inst) => inst.name.startsWith(search)).map((instruction) => (
+        {#each man.instructions.filter((i) => (search=='' | (i.mnemonic.toUpperCase().search(search.toUpperCase()) != -1) | (i.name.toUpperCase().search(search.toUpperCase()) != -1) | (i.description.toUpperCase().search(search.toUpperCase()) != -1) )) as instruction}
                 <div>
                     <div class="text-lg text-amber-100 flex gap-x-3 items-center ">
                         <span>{instruction.mnemonic}</span>
@@ -22,8 +24,8 @@ let search = "";
                     </div>
                     <p class="text-sm">{instruction.description}</p>
                 </div>
-            ))
-        }
+            
+        {/each}
     </div>
     <!-- <p class="">hello</p> -->
     <!-- <img class="self-end justify-self-end w-10" src="https://www.hatchwise.com/wp-content/uploads/2023/03/image-18.png.webp"/> -->
