29fc1046a4a236cfff422b0543417fa9ca4e52d6
NalinPlad <43052612+NalinPlad@users.noreply.github.com>
2024-06-26 14:09:07 -0700

    add RIP and default pointer location

--- src/components/AsmDisplay.svelte
+++ src/components/AsmDisplay.svelte
@@ -4,17 +4,30 @@
     import Highlight from "svelte-highlight";
     import x86asm from "svelte-highlight/languages/x86asm";
 
+    import {RIP} from '../registerStore';
+
+    // find address of _main first instruction
+    const main = asm_data.blocks.find((block) => block.name.match(/main/)).instructions[0].address;
+    
+    RIP.set(main);
+    
+    // console.log(main.instructions[0].address,16);
+
 </script>
 
 <div class="flex flex-col items-center bg-slate-900 p-5 gap-y-5 rounded-xl">
     <b class="font-mono">x86 INSTRUCTIONS</b>
     <!-- <div class="rounded"> -->
     <div class="flex">
-        <div class=" bg-slate-800 py-4 px-4 flex flex-col align-top gap-0 font-mono">
+        <div class=" bg-slate-800 py-4 px-4 flex flex-col gap-0 font-mono text-right">
             {#each asm_data.blocks as block }
                 <br/>
                 {#each block.instructions as instruction}
-                    <span class="">0x{instruction.address.replace("10000","…")+"\n"}</span>
+                    {#if instruction.address == $RIP}
+                        <span class="bg-slate-500 text-black px-3"><b>RIP </b>0x{instruction.address.replace("1000", "…")+"\n"}</span>
+                    {:else}
+                        <span class="">0x{instruction.address.replace("10000","…")+"\n"}</span>
+                    {/if}
                 {/each}
                 <br/>
             {/each}
---
+++ src/registerStore.js
@@ -0,0 +1,3 @@
+import { atom } from 'nanostores';
+
+export const RIP = atom(0x0);
