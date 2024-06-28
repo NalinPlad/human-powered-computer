381b912c123e5c3b27a644614182f132c334ffa5
NalinPlad <43052612+NalinPlad@users.noreply.github.com>
2024-06-27 19:53:29 -0700

    data validation push

--- src/components/CPUStack.svelte
+++ src/components/CPUStack.svelte
@@ -1,12 +1,27 @@
 <script>
     import {stack} from '../stackStore';
-    import {register_data} from '../registerStore';
+    import {register_data, getRegisterValue, setRegisterValue} from '../registerStore';
     
     String.prototype.isNumber = function(){return /^\d+$/.test(this);}
 
     let value = "";
 
     const registers = Object.keys(register_data.get());
+
+    function push() {
+        if (registers.includes(value.toUpperCase())) {
+            stack.set(getRegisterValue(value.toUpperCase()));
+        } else if (value.isNumber()) {
+            stack.set(parseInt(value));
+        }
+    }
+
+    function pop() {
+        if (registers.includes(value.toUpperCase())) {
+            setRegisterValue(value.toUpperCase(), stack.get());
+        }
+    
+    }
 </script>
 <div class="bg-green-200 border-green-500 border-8 rounded-xl p-5 shadow-xl h-fit flex flex-col items-center font-mono">
     <h1 class="text-green-950 bg-green-400 p-2 mb-3">CPYOU <b class="italic">STACK</b></h1>
@@ -15,10 +30,10 @@
         type="text"
         bind:value={value}
         placeholder="Enter register or value"
-        class=" rounded-lg p-2 bg-green-700 {(registers.includes(value.toUpperCase()) | value.isNumber()) ? "text-green-200" : "text-red-200"} font-bold placeholder:text-xs placeholder:text-white text-center mb-3"
+        class=" uppercase rounded-lg p-2 bg-green-700 {(registers.includes(value.toUpperCase()) | value.isNumber()) ? "text-green-200" : "text-red-200"} font-bold placeholder:text-xs placeholder:text-white text-center mb-3"
     />
     <div class="flex justify-center w-full gap-x-3">
-        <button class="btn">PUSH</button>
-        <button class="btn">POP</button>
+        <button on:click={push} class="btn {(registers.includes(value.toUpperCase()) | value.isNumber()) ? "text-green-200" : "text-red-200 cursor-not-allowed"}">PUSH</button>
+        <button on:click={pop} class="btn {registers.includes(value.toUpperCase()) ? "text-green-200" : "text-red-200 cursor-not-allowed"}">POP</button>
     </div>
 </div>
